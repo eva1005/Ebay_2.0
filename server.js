4a6a81b14a8297639b925d77d8f237a683b17f1e
Steffi1908 <91949263+Steffi1908@users.noreply.github.com>
2022-01-25 18:14:18 +0100

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/test", (req, res) => {
-        res.status(200).json({ success: true});
-});
-
-app.listen(1337, () => console.log("server is running on port 1337"));
