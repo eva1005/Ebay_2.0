17e7858c9c2dd9547c08ad5194e0b70fd9d4a621
eva1005 <77741604+eva1005@users.noreply.github.com>
2022-02-13 15:56:14 +0100

    update

--- Webprojekt/frontend/client/src/App.js
+++ Webprojekt/frontend/client/src/App.js
@@ -1,33 +1,32 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CreateArtikel from './Components/CreateArtikel';
 import Home from './Components/Home';
-
+import Login from './Components/Login';
+import SignIn from './Components/SignIn';
+import NewArtikel from './Components/NewArtikel';
 
 const App = () => {
 
-return(
-  <div className="app">
-    <BrowserRouter>
-      <header>
-        <main>
-          <Switch>
-          <Route exact path='./Components/Home' Components={Home} /><Home/>
-          <Route exact path='./CreateArtikel' Components={CreateArtikel} /><CreateArtikel/>
-          </Switch>
-        </main>
-      </header>
-    </BrowserRouter>
-    
-  </div>
-      );
+  return (
+    <div className="app">
+      <Router>
+        <header>
+          <main>
+            <Switch>
+              <Route exact path='/home' component={Home} />
+              <Route exact path='/createArtikel' component={CreateArtikel} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/signIn' component={SignIn} />
+              <Route exact path='/newArtikel' component={NewArtikel} />
+            </Switch>
+          </main>
+        </header>
+      </Router>
+
+    </div>
+  );
 };
-
-
-
-
-
-
 
 export default App;
--- Webprojekt/frontend/client/src/Components/Home.js
+++ Webprojekt/frontend/client/src/Components/Home.js
@@ -9,13 +9,15 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
+import { Link } from 'react-router-dom';
 import GlobalStyles from '@mui/material/GlobalStyles';
 import Container from '@mui/material/Container';
 import CardMedia from '@mui/material/CardMedia';
 import { styled, alpha } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
+import Stack from '@mui/material/Stack';
+import Avatar from '@mui/material/Avatar';
 
 // Copyright im Footer erstellen 
 function Copyright(props) {
@@ -31,71 +33,80 @@ const cards = [1, 2, 3, 4, 5, 6, 7, 8];
 
 // Such- Button 
 const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
-  }));
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
 
 const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }));
-  
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: '12ch',
-        '&:focus': {
-          width: '20ch',
-        },
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
       },
     },
-  }));
+  },
+}));
 
 
 // Footer
 const footers = [
-  {title: <a href="https://www.planb.net/">Unternehmen</a>,
-    description: [<a href="https://www.planb.net/">Team</a>, 
-    <a href="https://www.planb.net/">Geschichte</a>, 
-    <a href="https://www.planb.net/">Kontakt</a>, 
-    <a href="https://www.planb.net/">Standort</a>,],},
-  {title: <a href="https://www.planb.net/"> Produkte</a>,
+  {
+    title: <a href="https://www.planb.net/">Unternehmen</a>,
+    description: [<a href="https://www.planb.net/">Team</a>,
+    <a href="https://www.planb.net/">Geschichte</a>,
+    <a href="https://www.planb.net/">Kontakt</a>,
+    <a href="https://www.planb.net/">Standort</a>,],
+  },
+  {
+    title: <a href="https://www.planb.net/"> Produkte</a>,
     description: [<a href="https://www.planb.net/">Kategorien</a>,
-      <a href="https://www.planb.net/">Neue Produkte</a>,
-      <a href="https://www.planb.net/">Eigene Artikel</a>,],},
-  {title: <a href="https://www.planb.net/">Sozial Media</a>,
-    description: [<a href="https://www.planb.net/">Twitter</a>, 
-    <a href="https://www.planb.net/">Instargram </a>, 
-    <a href="https://www.planb.net/">Facebook</a>, 
-    <a href="https://www.planb.net/">LinkedIn</a>],},
-  {title: <a href="https://www.planb.net/">Richtlinien</a>,
-    description: [<a href="https://www.planb.net/">Datenschutz</a>, 
-    <a href="https://www.planb.net/">Impressum</a>],}];
+    <a href="https://www.planb.net/">Neue Produkte</a>,
+    <a href="https://www.planb.net/">Eigene Artikel</a>,],
+  },
+  {
+    title: <a href="https://www.planb.net/">Sozial Media</a>,
+    description: [<a href="https://www.planb.net/">Twitter</a>,
+    <a href="https://www.planb.net/">Instargram </a>,
+    <a href="https://www.planb.net/">Facebook</a>,
+    <a href="https://www.planb.net/">LinkedIn</a>],
+  },
+  {
+    title: <a href="https://www.planb.net/">Richtlinien</a>,
+    description: [<a href="https://www.planb.net/">Datenschutz</a>,
+    <a href="https://www.planb.net/">Impressum</a>],
+  }];
 
 function Home() {
   return (
     <React.Fragment>
+      {console.log('Test')}
       <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
       <CssBaseline />
       <AppBar
@@ -105,11 +116,11 @@ function Home() {
         sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
       >
         <Toolbar sx={{ flexWrap: 'wrap' }}>
+          <h2>E</h2><h4>bu</h4><h2>Y</h2>
           <Typography variant="h6" color="#468a84" noWrap sx={{ flexGrow: 1 }}>
-            EBUY
           </Typography>
-          <Button to='./CreateArtikel' component={Link}> Meine Artikel
-            </Button>
+          <Button variant="outlined" Primary to={'/CreateArtikel'} component={Link}> Meine Artikel
+          </Button>
           <Search>
             <SearchIconWrapper>
               <SearchIcon />
@@ -119,49 +130,52 @@ function Home() {
               inputProps={{ 'aria-label': 'search' }}
             />
           </Search>
-          <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
-            Login
+          <Button to={'/Login'} component={Link}>
+            <Stack direction="row" spacing={2}>
+              <Avatar src="/broken-image.jpg" />
+            </Stack>
           </Button>
         </Toolbar>
       </AppBar>
       <Container disableGutters maxWidth="sm" component="main" sx={{ pt: 8, pb: 6 }}>
-        <Typography variant="h2" align="center" color="#76aeb2" component="p">
-          Willkommen bei Ebuy! 
+        <Typography variant="h3" align="center" color="#76aeb2" component="p">
+          Unsere Artikel
         </Typography>
       </Container>
 
-{/*Produkt Vorschau*/}
+
+      {/*Produkt Vorschau*/}
       <Container sx={{ py: 2 }} maxWidth="none">
-          <Grid container spacing={7}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={4} md={3}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="img"
-                    image="https://source.unsplash.com/random"
-                    alt="random"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Artikel
-                    </Typography>
-                    <Typography>
-                      Beschreibung des Artikels!
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">Bieten</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
+        <Grid container spacing={7}>
+          {cards.map((card) => (
+            <Grid item key={card} xs={12} sm={4} md={3}>
+              <Card
+                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+              >
+                <CardMedia
+                  component="img"
+                  image="https://source.unsplash.com/random"
+                  alt="random"
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Artikel
+                  </Typography>
+                  <Typography>
+                    Beschreibung des Artikels!
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Bieten</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
 
 
-     
+
       <Container
         maxWidth="md"
         component="footer"
